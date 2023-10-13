defefc3dee6aa949bf6efc1bb71abe925d970000
CheoR <cheo.roman+github@gmail.com>
2023-10-13 10:58:26 -0500

    build(data): Schedule entity

--- app/src/main/java/com/example/busschedule/data/BusSchedule.kt
+++ app/src/main/java/com/example/busschedule/data/BusSchedule.kt
@@ -1,8 +1,15 @@
 package com.example.busschedule.data
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+/**
+ * Entity data class represents single [BusSchedule] database row.
+ */
+@Entity(tableName = "Schedule")
 data class BusSchedule(
+    @PrimaryKey(autoGenerate = true)
     val id: Int,
     val stopName: String,
     val arrivalTimeInMillis: Int
 )
-
